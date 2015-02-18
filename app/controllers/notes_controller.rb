1a1107c8ca8bcf70bf9a706fbdc2dceb86e0fb7f
David Dang <dtudang@gmail.com>
2015-02-18 15:26:21 -0800

    deletion works for deleting from database

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,11 +2,12 @@ class NotesController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
 def index
+  @notes = Note.all
   render json: Note.all
 end
 
 def new
-  @note = Note.news
+  @note = Note.new
 end
 
 def edit
@@ -16,6 +17,13 @@ def create
   @note = Note.new(note_params)
   if @note.save
     render:note
+  end
+end
+
+def destroy
+  @note = Note.find(params[:id])
+  if @note.destroy
+    render json: @note
   end
 end
 
